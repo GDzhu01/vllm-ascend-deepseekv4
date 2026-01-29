40cf98a91938de29758ae83eead9182ec486b853
LookAround <lixushi@huawei.com>
2026-01-29 17:47:57 +0800

    [bug fix] fix block_pool allocate block logic
    Signed-off-by: LookAround <lixushi@huawei.com>

--- vllm_ascend/patch/platform/patch_kv_cache_coordinator.py
+++ vllm_ascend/patch/platform/patch_kv_cache_coordinator.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
+from collections.abc import Sequence
 import vllm
 from vllm.v1.core.kv_cache_coordinator import (HybridKVCacheCoordinator,
                                                KVCacheCoordinator,
@@ -79,6 +80,35 @@ class KVCacheCoordinatorWithMultiPool(KVCacheCoordinator):
         blocks: tuple[list[KVCacheBlock], ...] = tuple(
             [] for _ in range(self.num_single_type_manager))
         return blocks, 0
+
+    def get_num_blocks_to_allocate(
+        self,
+        request_id: str,
+        num_tokens: int,
+        new_computed_blocks: tuple[Sequence[KVCacheBlock], ...],
+        num_encoder_tokens: int,
+    ) -> int:
+        """
+        Get the number of blocks needed to be allocated for the request.
+
+        Args:
+            request_id: The request ID.
+            num_tokens: The total number of tokens that need a slot (including
+                tokens that are already allocated).
+            new_computed_blocks: The new computed blocks just hitting the
+                prefix caching.
+            num_encoder_tokens: The number of encoder tokens for allocating
+                blocks for cross-attention.
+
+        Returns:
+            The number of blocks.
+        """
+        num_blocks_to_allocate = []
+        for i, manager in enumerate(self.single_type_managers):
+            num_blocks_to_allocate.append(manager.get_num_blocks_to_allocate(
+                request_id, num_tokens, new_computed_blocks[i]))
+        # We need to use the C128 block pool to check the number of blocks for allocation, as C128 is the bottleneck for the block count.
+        return num_blocks_to_allocate[1]
 
 
 def get_kv_cache_coordinator(
