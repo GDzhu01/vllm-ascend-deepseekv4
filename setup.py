44eace9f095b243620756d22a1f674bd9cde030b
maoxx241 <maomaoyu870@gmail.com>
2026-01-31 22:02:00 +0800

    fix build

--- setup.py
+++ setup.py
@@ -75,8 +75,8 @@ def get_value_from_lines(lines: List[str], key: str) -> str:
 
 def get_chip_type() -> str:
     try:
-        if env.SOC_VERSION is 'ascend910_95':
-            return env.SOC_VERSION
+        if envs.SOC_VERSION is 'ascend910_95':
+            return envs.SOC_VERSION
         npu_info_lines = subprocess.check_output(
             ['npu-smi', 'info', '-l']).decode().strip().split('\n')
         npu_id = int(get_value_from_lines(npu_info_lines, 'NPU ID'))
