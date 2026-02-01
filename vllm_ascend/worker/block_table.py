2ab54eb736ba25c227e77c692a516614fd13c4b0
nwpu-zxr <zhouxuerong2@huawei.com>
2026-02-01 20:04:56 +0800

    fix block table compress ratio
    Signed-off-by: nwpu-zxr <zhouxuerong2@huawei.com>

--- vllm_ascend/worker/block_table.py
+++ vllm_ascend/worker/block_table.py
@@ -27,9 +27,9 @@ class BlockTable:
                 hasattr(kv_cache_group, "kv_cache_spec") and \
                 hasattr(kv_cache_group.kv_cache_spec, "compress_ratio"):
             compress_ratio = kv_cache_group.kv_cache_spec.compress_ratio
-        self.max_num_blocks_per_req = max(max_num_blocks_per_req // compress_ratio, 1)
-        self.max_num_batched_tokens = max_num_batched_tokens // compress_ratio
-        max_num_blocks_per_req = max(max_num_blocks_per_req // compress_ratio, 1)
+        self.max_num_blocks_per_req = max(cdiv(max_num_blocks_per_req, compress_ratio), 1)
+        self.max_num_batched_tokens = cdiv(max_num_batched_tokens, compress_ratio)
+        max_num_blocks_per_req = max(cdiv(max_num_blocks_per_req, compress_ratio), 1)
         self.pin_memory = pin_memory
         self.device = device
         self.physical_block_size = block_size
