9bef6485f4e433894bf2c768c11f2a3182bf764b
zhaozixin (A) <z00842572@china.huawei.com>
2026-02-03 00:34:05 +0800

    add transpose batch matmul
    Signed-off-by: zhaozixin (A) <z00842572@china.huawei.com>

--- vllm_ascend/ops/linear.py
+++ vllm_ascend/ops/linear.py
@@ -33,6 +33,7 @@ from vllm.model_executor.layers.linear import (  # noqa
     ReplicatedLinear, RowParallelLinear, UnquantizedLinearMethod)
 from vllm.model_executor.layers.quantization.base_config import \
     QuantizationConfig
+from vllm.distributed import get_tensor_model_parallel_world_size
 from vllm.model_executor.utils import set_weight_attrs
 
 from vllm_ascend.ops.linear_op import get_parallel_op, get_replicated_op
@@ -381,6 +382,11 @@ class AscendColumnParallelLinear(ColumnParallelLinear):
 
         if self.custom_op is not None:
             self.custom_op.update_attrs()
+        self.prefix = prefix
+        if "wo_a" in prefix:
+            hf_config = get_current_vllm_config().model_config.hf_text_config
+            self.n_local_groups = getattr(hf_config, "o_groups", 0) // get_tensor_model_parallel_world_size()
+            self.o_lora_rank = getattr(hf_config, "o_lora_rank", 0)
 
     def forward(
         self,
@@ -390,6 +396,11 @@ class AscendColumnParallelLinear(ColumnParallelLinear):
             return self.custom_op.apply(input_)
 
         return super().forward(input_)
+    
+    def weight_loader(self, param: Parameter, loaded_weight: torch.Tensor):
+        super().weight_loader(param, loaded_weight)
+        if "wo_a" in self.prefix:
+           self.weight.data = self.weight.data.view(self.n_local_groups, self.o_lora_rank, -1).transpose(2,1).contiguous()
 
 
 class AscendReplicatedLinear(ReplicatedLinear):
