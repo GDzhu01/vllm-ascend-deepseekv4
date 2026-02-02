ba57e275e8bac8245ac559964e9781f040133c47
wxsIcey <1790571317@qq.com>
2026-02-02 10:25:39 +0000

    solve patch selector problem
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/patch/platform/__init__.py
+++ vllm_ascend/patch/platform/__init__.py
@@ -16,6 +16,7 @@
 
 import os
 
+import vllm_ascend.patch.worker.patch_selector # noqa
 import vllm_ascend.patch.platform.patch_distributed  # noqa
 import vllm_ascend.patch.platform.patch_ec_connector  # noqa
 import vllm_ascend.patch.platform.patch_kv_cache_utils  # noqa
