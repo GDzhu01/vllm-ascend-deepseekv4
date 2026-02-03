aa15c725e5722abd1c344bf15fc562fc6bdffbc0
wangxiyuan <wangxiyuan1007@gmail.com>
2026-02-03 16:41:52 +0800

    add new model
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>
    
    0204 new model

---
+++ vllm_ascend/attention/abstract.py
@@ -0,0 +1,58 @@
+
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, ClassVar, Generic, Protocol, TypeVar, get_args
+import torch
+
+from vllm.attention.backends.abstract import (
+    AttentionBackend,
+    AttentionImpl,
+    AttentionLayer,
+    AttentionType,
+    is_quantized_kv_cache,
+)
+
+class AttentionMetadata:
+    pass
+
+T = TypeVar("T", bound=AttentionMetadata)
+
+
+class DSAAttentionImpl(AttentionImpl[T], Generic[T]):
+    @abstractmethod
+    def __init__(
+        self,
+        dim: int,
+        n_heads: int,
+        scale: float,
+        n_local_heads: int,
+        o_lora_rank: int,
+        head_dim: int,
+        rope_head_dim: int | None,
+        nope_head_dim: int,
+        n_groups: int,
+        n_local_groups: int,
+        window_size: int,
+        compress_ratio: int,
+    ) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def forward(
+        self,
+        layer: AttentionLayer,
+        hidden_states_or_cq: torch.Tensor,
+        kv_c_normed: torch.Tensor,
+        k_pe: torch.Tensor,
+        kv_cache: torch.Tensor,
+        attn_metadata: T,
+        output: torch.Tensor | None = None,
+        output_scale: torch.Tensor | None = None,
+        output_block_scale: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        raise NotImplementedError
+
+def is_quantized_kv_cache(kv_cache_dtype: str) -> bool:
+    return kv_cache_dtype != "auto"
