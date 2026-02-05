eb6f7c1805c87b60f19508cc86b8e3e2acef3f56
nwpu-zxr <zhouxuerong2@huawei.com>
2026-02-06 03:19:37 +0800

    [P/D]Add mooncake_state_connector for multi-blockpool
    Signed-off-by: nwpu-zxr <zhouxuerong2@huawei.com>

--- vllm_ascend/distributed/__init__.py
+++ vllm_ascend/distributed/__init__.py
@@ -17,11 +17,13 @@
 
 from vllm.distributed.kv_transfer.kv_connector.factory import \
     KVConnectorFactory
+from vllm_ascend import envs
 
 
 def register_connector():
+    connector_path = "vllm_ascend.distributed.mooncake_state_connector" if envs.USE_MULTI_BLOCK_POOL else "vllm_ascend.distributed.mooncake_connector"
     KVConnectorFactory.register_connector(
-        "MooncakeConnectorV1", "vllm_ascend.distributed.mooncake_connector",
+        "MooncakeConnectorV1", connector_path,
         "MooncakeConnector")
 
     KVConnectorFactory.register_connector(
