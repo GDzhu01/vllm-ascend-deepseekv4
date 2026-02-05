a3818ac1eb39e07128a38ef0c73584090f52ae02
maoxx241 <maomaoyu870@gmail.com>
2026-02-06 03:57:48 +0800

    revert sas and sas metadata to 0204_2100 version

--- csrc/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
+++ csrc/attention/sparse_attn_sharedkv_metadata/op_kernel_aicpu/sparse_attn_sharedkv_metadata_aicpu.h
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the
+ * "License"). Please refer to the License for details. You may not use this
+ * file except in compliance with the License. THIS SOFTWARE IS PROVIDED ON AN
+ * "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS
+ * FOR A PARTICULAR PURPOSE. See LICENSE in the root of the software repository
+ * for the full text of the License.
  */
 
 /*!
@@ -45,8 +47,8 @@ enum class SparseMode : uint8_t {
 };
 
 enum class ValidSocVersion {
-    ASCEND910 = 0,
-    ASCEND950,
+    ASCEND910B = 0,
+    ASCEND910D,
     RESERVED_VERSION = 99999
 };
 
@@ -231,12 +233,6 @@ public:
 private:
     bool Prepare(CpuKernelContext &ctx);
     bool ParamsCheck();
-    void GetQueryBatchSize(uint32_t &bSize);
-    void GetKvBatchSize(uint32_t &bSize);
-    bool CheckSingleParam();
-    bool CheckExistence();
-    bool CheckConsistency();
-    bool CheckFeature();
     bool ParamsInit();
     bool BalanceSchedule(SplitResult &splitRes);
     bool GenMetaData(SplitResult &splitRes);
@@ -306,7 +302,7 @@ private:
     uint32_t headDim_ = 0;
     uint32_t oriTopK_ = 0;
     uint32_t cmpTopK_ = 0;
-    int32_t cmpRatio_ = -1;
+    uint32_t cmpRatio_ = -1;
     uint32_t oriMaskMode_ = 4;
     uint32_t cmpMaskMode_ = 3;
     int64_t winLeft_ = 127;
@@ -319,6 +315,7 @@ private:
     uint32_t aivCoreNum_ = 48U;
 
     // attr
+    uint32_t coreNum_ = 24U; // new
     std::string socVersion_ = "ascend910B";
     int64_t preToken_ = 0; // new
     int64_t nextToken_ = 0; // new
