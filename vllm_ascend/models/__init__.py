1d298643673d4b071464612d367ba7d43fdd0a6d
Yue <wangyue308@huawei.com>
2026-02-06 14:52:10 +0800

    bug fix

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -2,9 +2,9 @@ from vllm import ModelRegistry
 
 def register_model():
     ModelRegistry.register_model(
-    "DeepSeekNewForCausalLM",
-    "vllm_ascend.models.deepseek_new:AscendDeepSeekNewForCausalLM")
+    "DeepseekNewForCausalLM",
+    "vllm_ascend.models.deepseek_new:AscendDeepseekNewForCausalLM")
 
     ModelRegistry.register_model(
-    "DeepSeekNewMTPModel",
-    "vllm_ascend.models.deepseek_new_mtp:DeepSeekNewMTP")
+    "DeepseekNewMTPModel",
+    "vllm_ascend.models.deepseek_new_mtp:DeepseekNewMTP")
