6862c59c4c9c5672acb7e545686297376d5080e7
GDzhu01 <809721801@qq.com>
2026-04-17 16:53:59 +0800

    tmp fix compress ratio
    Signed-off-by: GDzhu01 <809721801@qq.com>

--- vllm_ascend/ops/fused_moe/moe_mlp.py
+++ vllm_ascend/ops/fused_moe/moe_mlp.py
@@ -275,7 +275,6 @@ def quant_apply_mlp(
             #     bias=bias1,
             #     use_mxfp_quant=use_mxfp_quant,
             # )
-            print(f'swiglu_limit is set to 10, remove it plz')
             hidden_states, swiglu_out_scale, _ = torch.ops._C_ascend.grouped_matmul_swiglu_quant_weight_nz(
                 x=hidden_states,
                 weight=_require_single_tensor_for_swiglu_quant(w1, name="w1"),
@@ -283,7 +282,7 @@ def quant_apply_mlp(
                 weight_scale=_require_single_tensor_for_swiglu_quant(w1_scale, name="w1_scale"),
                 x_scale=pertoken_scale,
                 bias=bias1,
-                swiglu_limit=10
+                swiglu_limit=10.0
             )
             if quantized_hidden_states is not None:
                 dispose_tensor(quantized_hidden_states)
