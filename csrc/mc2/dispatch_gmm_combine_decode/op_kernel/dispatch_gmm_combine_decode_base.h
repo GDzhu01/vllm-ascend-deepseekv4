6df4d23d37e04706bc85e911ffeb69bfd23a4ca5
wangxiyuan <wangxiyuan1007@gmail.com>
2026-04-23 21:02:30 +0800

    add new model
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

---
+++ csrc/mc2/dispatch_gmm_combine_decode/op_kernel/dispatch_gmm_combine_decode_base.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#ifndef DISPATCH_GMM_COMBINE_DECODE_BASE_H
+#define DISPATCH_GMM_COMBINE_DECODE_BASE_H
+
+<<<<<<< HEAD:csrc/dispatch_gmm_combine_decode/op_kernel/dispatch_gmm_combine_decode_base.h
+#include "moe_distribute_base.h"
+=======
+#include "../../../utils/inc/kernel/moe_distribute_base.h"
+>>>>>>> 5172bd48 (add new model):csrc/mc2/dispatch_gmm_combine_decode/op_kernel/dispatch_gmm_combine_decode_base.h
+
+#define TemplateMC2TypeClass typename ExpandXType, typename ExpandIdxType, bool IsNeedReduceScatter, uint32_t EXEC_FLAG
+#define TemplateMC2TypeFunc ExpandXType, ExpandIdxType, IsNeedReduceScatter, EXEC_FLAG
+#define TemplateDispatchTypeClass                                                                          \
+    typename XType, typename ExpandXOutType, bool StaticQuant, bool DynamicQuant, bool IsSmoothScaleExist, \
+        bool IsNeedAllgater, uint32_t EXEC_FLAG
+#define TemplateDispatchTypeFunc XType, ExpandXOutType, StaticQuant, DynamicQuant, IsSmoothScaleExist, IsNeedAllgater, EXEC_FLAG
+#endif  // DISPATCH_GMM_COMBINE_DECODE_BASE_H
