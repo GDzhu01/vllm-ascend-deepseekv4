c711353f05a2f7a974046fc0d7f7e2520e6548e1
ZT-AIA <1028681969@qq.com>
2026-04-24 00:14:39 +0800

    grouped_matmul_swiglu_quant_v2
    Signed-off-by: ZT-AIA <1028681969@qq.com>

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -54,6 +54,7 @@ elif [[ "$SOC_VERSION" =~ ^ascend910b ]]; then
         "rms_norm_dynamic_quant"
         "inplace_partial_rotary_mul"
         "grouped_matmul_swiglu_quant"
+        "grouped_matmul_swiglu_quant_v2"
     )
 
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
@@ -145,6 +146,7 @@ elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
         "rms_norm_dynamic_quant"
         "inplace_partial_rotary_mul"
         "grouped_matmul_swiglu_quant"
+        "grouped_matmul_swiglu_quant_v2"
     )
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
     SOC_ARG="ascend910_93"
