40af74a5c7205a875da0dca639a49086299d55f1
MengqingCao <qiuchunshuo@huawei.com>
2026-04-29 17:38:22 +0800

    tmp
    Signed-off-by: MengqingCao <qiuchunshuo@huawei.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -3018,11 +3018,21 @@ class NPUModelRunner(GPUModelRunner):
                         kv_cache_shape_list = [indexer_k_shape, indexer_scale_shape]
                         kv_cache_dtype_list = [current_kv_cache_spec.dtype, current_kv_cache_spec.scale_dtype]
 
+                    logger.info(100*"=")
+                    logger.info(f"before: {kv_cache.stride()=}")
+                    logger.info(f"before: {kv_cache.shape=}")
+
                     kv_cache = self._adjust_kv_layout(kv_tensor,
                                            kv_cache_shape_list,
                                            kv_cache_dtype_list,
                                            current_kv_cache_spec.page_size_bytes,
                                            )
+
+                    logger.info(f"after: {kv_cache.stride()=}")
+                    logger.info(f"after: {kv_cache.shape=}")
+                    logger.info(f"{kv_cache_shape_list=}")
+                    logger.info(f"{kv_cache_dtype_list=}")
+                    logger.info(100*"=")
 
                     kv_caches[layer_name] = kv_cache
                 # encounter OOM issue
